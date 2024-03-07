68795e5104f1bf630fc9f3d71d0bf379f4cca82d
katytapley <katy.tapley@gmail.com>
2024-03-07 14:26:59 -0600

    Created User and Bird classes and established a OneToMany relationship

---
+++ BirdWatcherApp/src/main/java/com/birdnerd/BirdWatcherApp/models/Bird.java
@@ -0,0 +1,28 @@
+package com.birdnerd.BirdWatcherApp.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class Bird {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+   //TODO: create private User birdSeen;
+}
--- BirdWatcherApp/src/main/java/com/birdnerd/BirdWatcherApp/models/User.java
+++ BirdWatcherApp/src/main/java/com/birdnerd/BirdWatcherApp/models/User.java
@@ -1,4 +1,34 @@
 package com.birdnerd.BirdWatcherApp.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "app_user")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String username;
+
+    private String password;
+
+    @OneToMany()
+    private List<Bird> birds = new ArrayList<>();
 }
