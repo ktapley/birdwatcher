f8b9414ec6209ac1fd4afa1c98f75da59ef8ef64
ktapley <124412792+ktapley@users.noreply.github.com>
2024-03-06 21:34:22 -0600

    Add files via upload

---
+++ BirdWatcherApp/src/main/java/com/birdnerd/BirdWatcherApp/BirdWatcherApp.java
@@ -0,0 +1,13 @@
+package com.birdnerd.BirdWatcherApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdWatcherApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirdWatcherApp.class, args);
+	}
+
+}
